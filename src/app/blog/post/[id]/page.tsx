467559c0c3792e76894a95e3686c2a8e11ac5cbd
Elsa Krisantana <elsakrisantana@Elsa-Macbook-Air.local>
2026-04-24 13:52:15 +0700

    fix: add post not found check

--- src/app/blog/post/[id]/page.tsx
+++ src/app/blog/post/[id]/page.tsx
@@ -1,11 +1,18 @@
-import { posts } from '@/app/lib/placeholder-data';
-import Post from '@/app/ui/components/posts/Post';
+import { posts } from "@/app/lib/placeholder-data";
+import Post from "@/app/ui/components/posts/Post";
 
 export default function Page({ params }: { params: { id: string } }) {
   const post = posts.find((post) => post.id === params.id);
+
+  // Add this check! ✅
+  if (!post) {
+    return <h1>Post not found!</h1>;
+  }
+
   return (
     <>
       <h1>Post</h1>
       <Post {...post} />
-    </>)
+    </>
+  );
 }
