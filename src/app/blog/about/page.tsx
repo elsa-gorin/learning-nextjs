6cdc19047953abfb31aeeb75d226b267e4141709
Elsa Krisantana <elsakrisantana@Elsa-Macbook-Air.local>
2026-04-24 14:53:29 +0700

    database connection: data.ts, env, posts/page.tsx

--- src/app/blog/about/page.tsx
+++ src/app/blog/about/page.tsx
@@ -1,23 +1,28 @@
-// export default function Page() {
-//   return (
-//     <>
-//       <h1 className="text-purple-800">About</h1>
-//       <p>Massa urna magnis dignissim id euismod porttitor vitae etiam viverra nunc at adipiscing sit morbi aliquet mauris porttitor nisi, senectus pharetra, ac porttitor orci.</p>
-//     </>)
-
-// }
-
-// src/app/page.tsx or any page
-import sql from "@/app/lib/db";
-
-export default async function Page() {
-  const users = await sql`SELECT * FROM users`;
-
+export default function Page() {
   return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>{user.name}</li>
-      ))}
-    </ul>
+    <>
+      <h1 className="text-purple-800">About</h1>
+      <p>
+        Massa urna magnis dignissim id euismod porttitor vitae etiam viverra
+        nunc at adipiscing sit morbi aliquet mauris porttitor nisi, senectus
+        pharetra, ac porttitor orci.
+      </p>
+    </>
   );
 }
+
+//src/app/page.tsx or any page
+
+// import sql from "@/app/lib/db";
+
+// export default async function Page() {
+//   const users = await sql`SELECT * FROM users`;
+
+//   return (
+//     <ul>
+//       {users.map((user) => (
+//         <li key={user.id}>{user.name}</li>
+//       ))}
+//     </ul>
+//   );
+// }
